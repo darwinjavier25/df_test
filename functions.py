d416c76b889b802c4bfdc60098f396e7c05b565d
darwinjavier25 <darwinjavier25@gmail.com>
2023-02-04 19:50:57 +0100

    Functions and join description

--- functions.py
+++ functions.py
@@ -20,15 +20,18 @@ def fibonacci(n):
 print(fibonacci(num))
 
 
-if num > 1:
-    for i in range(2, num):
-        if (num % i) == 0:
-            flag = True
-            break
+def prime(num):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = True
+                break
+    if flag:
+        print(num, "is not prime")
+    else:
+        print(num, "is prime")
+    return num
 
-if flag:
-    print(num, "is not prime")
-else:
-    print(num, "is prime")
 
+prime(num)
 
