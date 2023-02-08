ac00012b129df43078ac24d553a61f07299157ec
darwinjavier25 <darwinjavier25@gmail.com>
2023-02-08 17:34:43 +0100

    Functions better

--- functions.py
+++ functions.py
@@ -1,14 +1,11 @@
+num = 3
 def factorial(x):
     if x == 1:
         return 1
     else:
         return x * factorial(x - 1)
 
-
-num = 8
-flag = False
 print(factorial(num))
-
 
 def fibonacci(n):
     if n <= 1:
@@ -16,22 +13,20 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
-
 print(fibonacci(num))
 
-
-def prime(num):
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
+def prime(p):
+    flag = False
+    if p > 1:
+        for i in range(2, p):
+            if (p % i) == 0:
                 flag = True
                 break
     if flag:
-        print(num, "is not prime")
+        print(p, "is not prime")
     else:
-        print(num, "is prime")
-    return num
-
+        print(p, "is prime")
+    return p
 
 prime(num)
 
--- joins.py
+++ joins.py
@@ -1,6 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as func
-from pyspark.sql.functions import col, desc, first
 
 spark = SparkSession.builder.appName("df_test").getOrCreate()
 
@@ -21,9 +19,9 @@ print("Full Join: show all rows which join otherwise shows null")
 employe_df.join(dep_df, employe_df.emp_dept_id == dep_df.dept_id, "full").show()
 print("FullOuter Join: show all rows which join otherwise shows null")
 employe_df.join(dep_df, employe_df.emp_dept_id == dep_df.dept_id, "fullouter").show()
-print("Left or LeftOuter Join: show records which join and futhermore show records in left that not cross in right")
+print("Left or LeftOuter Join: show records which join and furthermore show records in left that not cross in right")
 employe_df.join(dep_df, employe_df.emp_dept_id == dep_df.dept_id, "left").show()
-print("Right or RightOuter Join: show records which join and futhermore show records in right that not cross in left")
+print("Right or RightOuter Join: show records which join and furthermore show records in right that not cross in left")
 employe_df.join(dep_df, employe_df.emp_dept_id == dep_df.dept_id, "right").show()
 print("LeftSemi Join: show records which are in left but not in right and only show left fields")
 employe_df.join(dep_df, employe_df.emp_dept_id == dep_df.dept_id, "leftsemi").show()
